173cfaddbf1bf32aefe478688fc4c6c5114b44d0
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-02-14 15:22:41 -0800

    init

---
+++ scripts/generate_python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if an argument (schema file path) is provided
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <path_to_schema_file.json>"
+  exit 1
+fi
+
+SCHEMA_FILE="$1"
+# Extract filename without extension, and extension
+SCHEMA_FILENAME=$(basename -- "$SCHEMA_FILE")
+SCHEMA_NAME="${SCHEMA_FILENAME%.*}"
+
+# Define the output file path, using the schema name for the Python model file
+OUTPUT_FILE="src/python/langtrace/trace-attributes/models/${SCHEMA_NAME}.py"
+
+# Generate the Python model
+datamodel-codegen --input "$SCHEMA_FILE" --input-file-type jsonschema --output "$OUTPUT_FILE"
+
+echo "Generated Python model at: $OUTPUT_FILE"
---
+++ scripts/generate_typescript.ts
@@ -0,0 +1,36 @@
+// Modified convertSchemaToInterface.ts
+import * as fs from "fs";
+import * as TJS from "json-schema-to-typescript";
+import * as path from "path";
+
+// Check for the presence of the command line argument
+if (process.argv.length < 3) {
+  console.log(
+    "Usage: ts-node scripts/schema_to_interface.ts src/<vendor>_span_attributes.json"
+  );
+  process.exit(1);
+}
+
+// Take the schema file path from the command line arguments
+const schemaFilePath = process.argv[2];
+const schemaDir = path.dirname(schemaFilePath);
+const schemaBaseName = path.basename(
+  schemaFilePath,
+  path.extname(schemaFilePath)
+);
+
+// Define the output TypeScript file path
+const outputFilePath = path.join(
+  "generated/typescript",
+  `${schemaBaseName}.d.ts`
+);
+
+// Read and parse the JSON Schema
+const schema = JSON.parse(fs.readFileSync(schemaFilePath, "utf8"));
+
+// Compile the JSON Schema to a TypeScript interface
+TJS.compile(schema, schemaBaseName)
+  .then((ts) => fs.writeFileSync(outputFilePath, ts))
+  .catch((error) => console.error(error));
+
+console.log(`TypeScript definition generated at: ${outputFilePath}`);
