993f351999937f9e739f912f2a8c5871587891a7
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-02-15 10:39:10 -0800

    remove span events

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,3 +1,2 @@
 export { LangTraceSpanAttributes } from "./models/langtrace_span_attributes";
 export { OpenAISpanAttributes } from "./models/openai_span_attributes";
-export { OpenAISpanEvents } from "./models/openai_span_events";
