dc975cf9cd3debedbe9200bb8e49f9ffd9bfac46
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-02-14 21:46:58 -0800

    fix

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import find_packages, setup
 
 setup(
     name='trace-attributes',  # Choose a unique name for PyPI
-    version='1.0.0',
+    version='1.0.7',
     author='Karthik Kalyanaraman',
     author_email='karthik@scale3labs.com',
     description='LangTrace - Trace Attributes',
-    long_description=open('README.md').read(),
+    long_description="LangTrace - Trace Attributes",
     long_description_content_type='text/markdown',
     url='https://github.com/Scale3-Labs/langtrace-trace-attributes',  # Project home page
     package_dir={'': 'src/python'},
     packages=find_packages(where='src/python'),
     install_requires=[
         'pydantic>=1.8',  # Example dependency, adjust according to your project's needs
-        # Other dependencies
+        'typing'
     ],
     python_requires='>=3.6',
     classifiers=[
@@ -21,5 +21,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    include_package_data=True,  # To include non-code files specified in MANIFEST.in
+    include_package_data=False,  # To include non-code files specified in MANIFEST.in
 )
