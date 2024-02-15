77f45ca0301a43249cd273cc8d3d3792e02eb046
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-02-14 23:23:22 -0800

    fixes

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,2 +1,3 @@
 export { LangTraceSpanAttributes } from "./models/langtrace_span_attributes";
 export { OpenAISpanAttributes } from "./models/openai_span_attributes";
+export { OpenAISpanEvents } from "./models/openai_span_events";
