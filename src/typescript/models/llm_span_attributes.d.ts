185dc74d4294da59abad366bef1aa1d8caf0bf88
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-04-23 16:32:13 -0700

    Add retrieval model params

--- src/typescript/models/llm_span_attributes.d.ts
+++ src/typescript/models/llm_span_attributes.d.ts
@@ -19,7 +19,7 @@ export interface LLMSpanAttributes {
   "llm.top_k"?: number;
   "llm.user"?: string;
   "llm.system.fingerprint"?: string;
-  "llm.prompts": string;
+  "llm.prompts"?: string;
   "llm.function.prompts"?: string;
   "llm.responses"?: string;
   "llm.token.counts"?: string;
@@ -45,6 +45,8 @@ export interface LLMSpanAttributes {
   "llm.embedding_dataset_id"?: string;
   "llm.embedding_input_type"?: string;
   "llm.embedding_job_name"?: string;
+  "llm.retrieval.query"?: string;
+  "llm.retrieval.results"?: string;
   "user.id"?: string;
   "user.feedback.rating"?: number;
   "http.max.retries"?: number;
