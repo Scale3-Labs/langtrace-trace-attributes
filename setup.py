d2635959865db9a0ca2939f98a6c378b17e2d364
Ali Waleed <134522290+alizenhom@users.noreply.github.com>
2024-05-29 16:55:05 +0300

    remove typing dependecy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='trace-attributes',  # Choose a unique name for PyPI
-    version='4.0.2',
+    version='4.0.3',
     author='Karthik Kalyanaraman',
     author_email='karthik@scale3labs.com',
     description='LangTrace - Trace Attributes',
@@ -13,7 +13,6 @@ setup(
     packages=find_packages(where='src/python'),
     install_requires=[
         'pydantic>=1.8',  # Example dependency, adjust according to your project's needs
-        'typing'
     ],
     python_requires='>=3.6',
     classifiers=[
