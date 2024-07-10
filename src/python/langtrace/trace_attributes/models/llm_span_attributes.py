d50f07f30b8740a5d6250c3958aa77d76c380a58
Ali Waleed <134522290+alizenhom@users.noreply.github.com>
2024-07-10 19:33:56 +0300

    fix allow dict

--- src/python/langtrace/trace_attributes/models/llm_span_attributes.py
+++ src/python/langtrace/trace_attributes/models/llm_span_attributes.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 from typing import List, Optional
 
-from pydantic import BaseModel, Extra, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class LLMSpanAttributes(BaseModel):
