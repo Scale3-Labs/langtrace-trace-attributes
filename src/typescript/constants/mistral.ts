5951a495ffad46e05f30a477c82b9d73d035d556
Obinna Okafor <obinna.okafor01@gmail.com>
2024-09-04 14:17:39 +0100

    add mistral

---
+++ src/typescript/constants/mistral.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Scale3 Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const APIS = {
+  CHAT_COMPLETE: {
+    METHOD: 'mistral.chat.complete',
+    ENDPOINT: '/v1/chat/completions'
+  },
+  CHAT_STREAM: {
+    METHOD: 'mistral.chat.stream',
+    ENDPOINT: '/v1/chat/completions'
+  },
+  EMBEDDINGS_CREATE: {
+    METHOD: 'openai.embeddings.create',
+    ENDPOINT: '/v1/embeddings'
+  }
+} as const
+export type MistralFunctions = typeof APIS[keyof typeof APIS]['METHOD']
+export const MistralFunctionNames: MistralFunctions[] = Object.values(APIS).map((api) => api.METHOD)
